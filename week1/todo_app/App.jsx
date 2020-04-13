6029a1311b887284a2e3bb4f46599a22920b4c98
Sabrina Lewis <sabrina.lewis@bryanuniversity.edu>
2020-04-12 21:25:10 -0500

    my-app and todo app

---
+++ week1/todo_app/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <input type="checkbox"/>
+            <p>checkbox 1</p>
+            
+            <input type="checkbox"/>
+            <p>checkbox 2</p>
+            
+            <input type="checkbox"/>
+            <p>checkbox 3</p>
+        </div>
+    )
+}
+
+export default App
